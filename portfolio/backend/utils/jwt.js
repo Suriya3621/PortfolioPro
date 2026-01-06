3778ec75b174ddd72d34ec8159ef656ef7f5a432
Suriya3621 <suriyaprakashraja849@gmail.com>
2026-01-06 22:25:44 +0530

    jwt samesite is none

--- portfolio/backend/utils/jwt.js
+++ portfolio/backend/utils/jwt.js
@@ -6,7 +6,7 @@ const sendToken = (res, user) => {
       Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    sameSite: "lax",
+    sameSite: "none",
     secure: true,
   };
   res.cookie("token", token, options);
